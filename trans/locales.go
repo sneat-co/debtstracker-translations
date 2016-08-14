531ed672b26022e1eb0be7ffefa70dde010abc71
Alexander Trakhimenok <alexander.trakhimenok@gmail.com>
2016-08-14 12:57:31 +0100

    another step forward

--- trans/locales.go
+++ trans/locales.go
@@ -22,20 +22,20 @@ var SupportedLocalesByCode5 = map[string]strongo.Locale{
 	strongo.LOCALE_EN_US: strongo.LocaleEnUS,
 	//strongo.LOCALE_EN_UK: strongo.LocaleEnUK,
 	strongo.LOCALE_RU_RU: strongo.LocaleRuRu,
-	strongo.LOCALE_IT_IT: strongo.LocaleItIt,
+	//strongo.LOCALE_IT_IT: strongo.LocaleItIt,
 	//strongo.LOCALE_ID_ID: strongo.LocaleIdId,
 	//strongo.LOCALE_DE_DE: strongo.LocaleDeDe,
 	//strongo.LOCALE_ES_ES: strongo.LocaleEsEs,
 	//strongo.LOCALE_FR_FR: strongo.LocaleFrFr,
 	//strongo.LOCALE_PT_PT: strongo.LocalePtPt,
 	//strongo.LOCALE_PT_BR: strongo.LocalePtBr,
-	strongo.LOCALE_FA_IR: strongo.LocaleFaIr,
+	//strongo.LOCALE_FA_IR: strongo.LocaleFaIr,
 }
 
 var SupportedLocales []strongo.Locale = []strongo.Locale{
 	strongo.LocaleEnUS,
 	strongo.LocaleRuRu,
-	//strongo.LocaleIdId,
+	strongo.LocaleIdId,
 	strongo.LocaleFaIr,
 	strongo.LocaleItIt,
 	//LocaleDeDe,
